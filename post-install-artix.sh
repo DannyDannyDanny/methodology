7524ac8a196e9fc4e3e5ec7dc74a73247ab89101
Daniel Thoren <powerhouseplayer@gmail.com>
2022-03-27 01:32:39 +0100

    :boom: add yay installation

--- post-install-artix.sh
+++ post-install-artix.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
+# run as root
 pacman -Syy
 pacman -Syu
+
+# the following commands are run manually
+pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
