42cf95f69bb1dac057a484a7a85268ab2cbd85c6
Daniel Thoren <powerhouseplayer@gmail.com>
2022-03-28 16:39:51 +0200

    :memo: add link to install script

--- post-install-artix.sh
+++ post-install-artix.sh
@@ -10,13 +10,16 @@ sudo pacman -Syu
 # install basic shidd
 sudo pacman -S --needed git base-devel
 
-# 
+# install AUR helper
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 
 # install programs
 yay -S librewolf-bin
+
+# install zsh and omz and more
+# https://medium.com/tech-notes-and-geek-stuff/install-zsh-on-arch-linux-manjaro-and-make-it-your-default-shell-b0098b756a7a
 
 
 # kill the beep sound
