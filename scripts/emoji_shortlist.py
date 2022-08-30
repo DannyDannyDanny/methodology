b06d9d9fccfc8a0d5e4340686c29d2786811a51c
dth@taiga.ai <DannyDannyDanny>
2022-08-30 09:46:35 +0200

    vimwiki: dth @ dth-MacBookAir

---
+++ scripts/emoji_shortlist.py
@@ -0,0 +1,13 @@
+"""prints my favorite emojis"""
+
+* fetch all emojis
+* filter by shortcode
+  * max string length: 7
+  * must be in dictionary:
+"""
+
+def is_in_dict():
+  return True
+
+def is_in_dict():
+  return True
